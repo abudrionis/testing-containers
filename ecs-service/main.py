6f05d3fcbd9782ebc0a657a4dd59cabfcb0904fd
Andrius Budrionis <andrius.budrionis@gmail.com>
2022-04-07 13:29:59 +0200

    healthcheck endpoint added to the ecs-service

--- ecs-service/main.py
+++ ecs-service/main.py
@@ -1,6 +1,5 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
@@ -8,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/health", response_class=PlainTextResponse)
+def healthcheck():
+    return "200"
